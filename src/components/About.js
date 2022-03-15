ca9e585959075b8acba9c5a495a0e8461341ed9b
Skyz03 <aakibdiscoo@gmail.com>
2022-03-15 20:16:55 +0545

    Updates in the about section

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,16 @@ import React from "react";
 const About = () => {
     return (
         <div>
-           <p> this is the about section</p>
+        <div className="card__para">
+           <h3 className="title">About</h3>
+           <p className="des">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
         </div>
+
+    <div className="card__para mt-3">
+        <h3 className="title">Interests</h3>
+        <p className="des">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+    </div>
+    </div>
     )
 }
 
