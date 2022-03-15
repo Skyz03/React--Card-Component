9df75df8b81d9f74a7554d5804a42709a131b153
Skyz03 <aakibdiscoo@gmail.com>
2022-03-15 19:56:20 +0545

    Updates in the name section of the card

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import Info from "./components/Info";
 import About from "./components/About";
 import Footer from "./components/Footer";
+import '../src/App.scss'
 
 const App = () => {
   return(
-    <div>
-      <Info />
-      <About />
-      <Footer />
+    <div className="container">
+      <div className="card">
+        <Info />
+        <About />
+        <Footer />
+      </div>
     </div>
   )
 }
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,12 +1,18 @@
 import React from "react";
-
+import pic from "../pic.png";
 
 const Info = () => {
-    return (
-        <div>
-           <p> this is the info section</p>
-        </div>
-    )
-}
+  return (
+    <div>
+      <img src={pic} alt='profile-pic' />
+
+      <div className='card__text'>
+        <h2 className="card__name">Laura Smith</h2>
+        <p className="card__position">FrontEnd Developer</p>
+        <p className="card__site">laurasmith.website</p>
+      </div>
+    </div>
+  );
+};
 
 export default Info;
