80c2f410ffe08db81488474e7ee5c019dce8e757
Skyz03 <aakibdiscoo@gmail.com>
2022-03-16 10:10:10 +0545

    Updates in the footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,11 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <div>
-           <p> this is the Footer section</p>
+        <div className="card__icons">
+            <i class="fa-brands fa-twitter-square fa-2xl"></i>
+            <i class="fa-brands fa-facebook-square fa-2xl"></i>
+            <i class="fa-brands fa-instagram-square fa-2xl"></i>
+            <i class="fa-brands fa-github-square fa-2xl"></i>
         </div>
     )
 }
